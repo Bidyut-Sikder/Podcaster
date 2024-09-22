9696a9cd1abe9eaf8b87cf4f3c3f7b35ec5ea03a
Bidyut-Sikder <bidyutsikder2001@gmail.com>
2024-09-22 23:35:18 +0600

    setting uploader methods

--- convex/openai.ts
+++ convex/openai.ts
@@ -1,44 +1,23 @@
-
-
-
 import { action } from "./_generated/server";
 import { v } from "convex/values";
+import OpenAI from "openai";
+import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
 
-export const generateAudioAction = action({
-  args: { a: v.number(), b: v.number() },
-  handler: (_, args) => {
-    // do something with `args.a` and `args.b`
-
-    // optionally return a value
-    return "success";
-  },
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
+export const generateAudioAction = action({
+  args: { input: v.string(), voice: v.string() },
+  handler: async (_, { voice, input }) => {
+    const mp3 = await openai.audio.speech.create({
+      model: "tts-1",
+      voice: voice as SpeechCreateParams["voice"],
+      input,
+    });
 
+    const buffer = await mp3.arrayBuffer();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return buffer;
+  },
+});
